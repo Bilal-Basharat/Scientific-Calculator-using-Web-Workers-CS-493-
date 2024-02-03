4a9e330de9df4749681d2bcfb6a1c479b9e3d578
Bilal Basharat <bilalbisharat@gmail.com>
2024-02-03 15:46:08 +0500

    Create footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+       <footer className="mb-0">
+        <p>Powered by Bilal Basharat &hearts;</p>
+       </footer>
+    </>
+  )
+}
