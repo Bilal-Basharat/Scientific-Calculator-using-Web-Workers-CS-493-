df635f0324a3484eb8d514f739d1c371fc3725f3
Bilal Basharat <bilalbisharat@gmail.com>
2024-02-03 15:50:12 +0500

    Create calculator.js to implement the logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import { Calculator } from './components/Calculator';
+import { Footer } from './components/footer';
+
 
 function App() {
   return (
